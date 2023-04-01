c9e837828ffccb196bfea3b577aeffa9f6d95ecb
Fatih Şükran <fatih.sukran@enuygun.com>
2023-04-01 08:10:07 +0300

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Fatih Şükran on 1.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
